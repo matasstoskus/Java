400d3a494431b277c72657566000ac209a94ba25
Mac Matas Air Stoskus <mac@MacBook-Air.Dlink>
2020-09-17 11:47:40 +0300

    Adding classwork

---
+++ src/gg/matas/app0/inClass1/InClass1.java
@@ -0,0 +1,12 @@
+package gg.matas.app0.inClass1;
+
+public class InClass1 {
+    public static void main(String[] args) {
+        Dog dog0 = new Dog();
+        dog0.setName("bugis");
+        dog0.setVeisle("labradoras");
+        System.out.println(dog0.getName() + " " + dog0.getVeisle());
+        Dog dog1 = new Dog("baitas", "terjeras");
+        System.out.println(dog1.getName() + " " + dog1.getVeisle());
+    }
+}
